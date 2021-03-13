f109cafa47d47d5e41c85372d25ec6ba39fa236a
Jocelyn <joeads@iu.edu>
2021-03-13 04:53:13 -0500

    feat: Set up basic 3D grid
    Enum for tile type is randomly selected on start, appropriate material is assigned

---
+++ Grid/Assets/Scripts/GridManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GridManager : MonoBehaviour
+{
+    public int rows = 10;
+    public int columns = 10;
+    public int scale = 1;
+
+    public GameObject tilePrefab;
+    public Vector3 bottomLeftCoords = new Vector3(0, 0, 0);
+
+    [SerializeField]
+    public GameObject[,] gridArray;
+
+    
+    void Awake()
+    {
+        gridArray = new GameObject[columns, rows];
+
+        if (tilePrefab)
+        {
+            GenerateGrid();
+        }
+        else print("Missing TilePrefab");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void GenerateGrid()
+    {
+        for (int i = 0; i < columns; i++)
+        {
+            for (int j = 0; j < rows; j++)
+            {
+                GameObject obj = Instantiate(tilePrefab, new Vector3(bottomLeftCoords.x + scale * i, bottomLeftCoords.y, bottomLeftCoords.z + scale + scale * j), Quaternion.identity);
+                obj.transform.SetParent(gameObject.transform);
+                obj.GetComponent<TileStat>().xPos = i;
+                obj.GetComponent<TileStat>().yPos = j;
+
+                gridArray[i, j] = obj;
+            }
+        }
+    }
+
+
+}
---
+++ Grid/Assets/Scripts/TileStat.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum TileType { Grass, Dirt, Water }
+public class TileStat : MonoBehaviour
+{
+    public int visited = -1;
+    public int xPos = 0;
+    public int yPos = 0;
+
+    public Material[] materials = new Material[3];
+    public Material currentMaterial;
+
+    [SerializeField]
+    
+    public TileType currentType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentType = (TileType)Random.Range(0, 3);
+
+        if (currentType == TileType.Dirt)
+        {
+            currentMaterial = materials[0];
+        }
+        if (currentType == TileType.Grass)
+        {
+            currentMaterial = materials[1];
+        }
+        if (currentType == TileType.Water)
+        {
+            currentMaterial = materials[2];
+        }
+
+        GetComponent<Renderer>().material = currentMaterial;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
